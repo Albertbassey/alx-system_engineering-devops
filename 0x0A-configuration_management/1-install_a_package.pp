b02547f55ea1adacfb83683d116471308ed87f30
Albertbassey <aniediongetim@gmail.com>
2024-05-03 21:38:59 -0700

    1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,5 @@
-
-# Install a package using Puppet
-
-exec { 'puppet-lint':
-  command => '/bin/gem install puppet-lint -v 2.5.0',
+# Using Puppet, install flask from pip3
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
-
